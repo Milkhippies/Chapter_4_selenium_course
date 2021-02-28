69c3b5f62e932d1866266383f369a8e3ee88c590
Dima Poryadin <Hippipi1@hotmail.com>
2021-02-28 15:43:55 +0300

    write test_login_page

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,13 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators():
+    REGISTRATION_MAIL = (By.ID, "id_registration-email")
+    REGISTRATION_PASSWORD = (By.ID, "id_registration-password1")
+    REGISTRATION_CONFIRM = (By.ID, "id_registration-password2")
+    REGISTRATION_BUTTON = (By.CSS_SELECTOR, "#register_form > button:nth-child(7)")
+    LOGIN_MAIL = (By.ID, "id_login-username")
+    LOGIN_PASSWORD = (By.ID, "id_login-password")
+    LOGIN_BUTTON = (By.CSS_SELECTOR, "#login_form > button:nth-child(7)")
---
+++ pages/login_page.py
@@ -0,0 +1,31 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+# link = http://selenium1py.pythonanywhere.com/ru/accounts/login/
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert "login" in self.browser.current_url, "'login' not in current url"
+
+    def should_be_login_form(self):
+        loginMail = self.browser.find_element(*LoginPageLocators.LOGIN_MAIL)
+        loginPassword = self.browser.find_element(*LoginPageLocators.LOGIN_PASSWORD)
+        loginButton = self.browser.find_element(*LoginPageLocators.LOGIN_BUTTON)
+        loginButton.click()
+        # реализуйте проверку, что есть форма логина
+        assert True, "ошибка в форме логина"
+
+    def should_be_register_form(self):
+        registrationMail = self.browser.find_element(*LoginPageLocators.REGISTRATION_MAIL)
+        registrationPassword = self.browser.find_element(*LoginPageLocators.REGISTRATION_PASSWORD)
+        registrationConfirmPassword = self.browser.find_element(*LoginPageLocators.REGISTRATION_CONFIRM)
+        registrationButton = self.browser.find_element(*LoginPageLocators.REGISTRATION_BUTTON)
+        registrationButton.click()
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert True, "ошибка в форме регистрации"
